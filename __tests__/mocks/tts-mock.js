a202e46a3dc6d17365f4958f43265fbdf7aa563e
IceFog72 <164350516+IceFog72@users.noreply.github.com>
2026-03-24 05:59:30 +0200

    Delete __tests__ directory

--- __tests__/mocks/tts-mock.js
+++
@@ -1,5 +0,0 @@
-// Mock for SillyTavern TTS index.js
-export function saveTtsProviderSettings() {}
-export function getPreviewString(lang) {
-    return 'The quick brown fox jumps over the lazy dog';
-}
